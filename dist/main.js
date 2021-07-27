e086377fc1ca1418451c0fc347df0f715defb3c6
Williams Samuel <samwill300@gmail.com>
2021-07-27 11:35:43 +0100

    worked on the number of people input field

--- dist/main.js
+++ dist/main.js
@@ -1,31 +1,41 @@
+// DOM Elements
 const bill = document.getElementById('bill')
-const numberOfPeople = document.getElementById('people')
+const people = document.getElementById('people')
 const tipPercents = document.querySelectorAll('input[type="radio"]')
 const resetBtn = document.querySelector('.reset-btn')
 const tipAmount = document.querySelector('.tips')
 const perPerson = document.querySelector('.per-person')
 const customTip = document.querySelector('.custom-tip')
 
-function tipCalculator(billValue, calculateTip, people = 1) {
+// intial number of people (value)
+people.value = 1
+
+function tipCalculator(billValue, calculateTip, numberOfPeople) {
 	const calculatedTip = (billValue * calculateTip).toPrecision(3)
-	const totalAmount = (billValue * (people + calculateTip)).toPrecision(4)
+	const totalAmount = (
+		billValue *
+		(numberOfPeople + calculateTip)
+	).toPrecision(4)
 
 	tipAmount.textContent = `$${calculatedTip}`
 	perPerson.textContent = `$${totalAmount}`
 }
 
+// calculate tip based on the number of people and bill
 function calculateTip(e) {
 	if (bill.value === '') return
 	const billValue = +bill.value
+	const numberOfPeople = +people.value
 
 	const tip = e.target.dataset.tip
 
 	const calculateTip = tip / 100
 
-	tipCalculator(billValue, calculateTip)
+	tipCalculator(billValue, calculateTip, numberOfPeople)
 	resetBtn.removeAttribute('disabled', 'disabled')
 }
 
+// calculate user custom tip percentage
 function calculateCustomTip(e) {
 	if (bill.value === '' || customTip.value === '') return
 
@@ -37,18 +47,14 @@ function calculateCustomTip(e) {
 	tipCalculator(billValue, calculateTip)
 }
 
+// reset all fields
 function resetAll() {
 	bill.value = ''
 	customTip.value = ''
+	people.value = 1
 	tipAmount.textContent = `$00.00`
 	perPerson.textContent = `$00.00`
 	resetBtn.setAttribute('disabled', 'disabled')
-
-	tipPercents.forEach((tipPercent) =>
-		tipPercent.addEventListener('click', (e) =>
-			e.target.removeAttribute('checked', 'checked')
-		)
-	)
 }
 
 // event listeners
